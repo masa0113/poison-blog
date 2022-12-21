058a0d9ea59216c06a750da653a7a7aebd4e648d
masahiro.fukuyama <be.fukuyama.m@gmail.com>
2022-12-21 14:51:42 +0900

    build error解消

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,23 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   const theme = extendTheme({
     styles: {
       global: {
         body: {
-          bg: "gray.100",
-          color: "gray.800",
-          maxWidth: "1280px",
-          margin: "0 auto",
-          p: '20px 0'
+          bg: 'gray.100',
+          color: 'gray.800',
+          maxWidth: '1280px',
+          margin: '0 auto',
+          p: '20px 0',
         },
       },
     },
-  });
+  })
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  );
+  )
 }
